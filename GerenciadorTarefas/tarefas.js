276323993ab9779ad0fec7cac9750b221029e48e
dudamegc <megacosta10@gmail.com>
2026-03-11 23:06:56 -0300

    Iniciando css

---
+++ GerenciadorTarefas/tarefas.js
@@ -0,0 +1,10 @@
+// configurações dos níveis do progresso
+
+const niveis = [
+    {nome:"Peão Iniciante", peça:"♟", porcenMin: 0.00}, //Nível 1
+    {nome: "Torre Ascendente", peça: "♜", porcenMin: 0.20}, //Nível 2
+    {nome: "Cavalo Veloz", peça: "♞", porcenMin: 0.40}, //Nível 3
+    {nome: "Bispo Iluminado", peça: "♝", porcenMin: 0.60}, //Nível 4
+    {nome: "Rainha Poderosa", peça: "♛", porcenMin: 0.80}, //Nível 5
+    {nome: "CHECKMATE", peça: "♚", porcenMin: 1.00} //Nível 6
+];
