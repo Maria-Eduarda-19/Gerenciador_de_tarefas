97c52b772cb6d368f07e59c7495521bfb04aac48
Maria-Eduarda-19 <dudacalado2005@gmail.com>
2026-03-14 16:56:43 -0300

    Atualizando layout e funcionalidade da lista de tarefas

--- GerenciadorTarefas/tarefas.js
+++ GerenciadorTarefas/tarefas.js
@@ -8,3 +8,77 @@ const niveis = [
     {nome: "Rainha Poderosa", peça: "♛", porcenMin: 0.80}, //Nível 5
     {nome: "CHECKMATE", peça: "♚", porcenMin: 1.00} //Nível 6
 ];
+
+function addTask(){
+
+    const input = document.getElementById("taskInput");
+    const text = input.value;
+
+    if(text === "") return;
+
+    const li = document.createElement("li");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    const span = document.createElement("span");
+    span.textContent = text;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "🗑";
+
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+
+    document.getElementById("pendingList").appendChild(li);
+
+    input.value = "";
+
+    verificarLista();
+
+    checkbox.addEventListener("change", function(){
+
+    if(this.checked){
+        document.getElementById("completedList").appendChild(li);
+    }else{
+        document.getElementById("pendingList").appendChild(li);
+    }
+
+    verificarLista();
+    verificarConcluidos();
+
+});
+
+   deleteBtn.addEventListener("click", function(){
+    li.remove();
+    verificarLista();
+    verificarConcluidos();
+});
+
+}
+
+function verificarLista(){
+
+    const lista = document.getElementById("pendingList");
+    const mensagem = document.getElementById("mensag");
+
+    if(lista.children.length === 0){
+        mensagem.style.display = "block";
+    }else{
+        mensagem.style.display = "none";
+    }
+
+}
+function verificarConcluidos(){
+
+    const lista = document.getElementById("completedList");
+    const mensagem = document.getElementById("mensagc");
+
+    if(lista.children.length === 0){
+        mensagem.style.display = "block";
+    }else{
+        mensagem.style.display = "none";
+    }
+
+}
